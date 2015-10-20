f0fbf101856774d11629bafcf49058699c29f616
Abdul Munim <iam@munim.net>
2015-10-20 11:43:13 +0100

    Added blank MVC project

---
+++ src/CrowdyFunding.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrowdyFunding.Mvc.Startup))]
+namespace CrowdyFunding.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
